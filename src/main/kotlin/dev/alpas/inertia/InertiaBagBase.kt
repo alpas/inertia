b817401f4d9693914e1e7a9d5acef8168dfeeaa0
Ashok Gelal <ashokgelal@gmail.com>
2020-04-09 11:17:01 -0600

    Pass the csrf_token

--- src/main/kotlin/dev/alpas/inertia/InertiaBagBase.kt
+++ src/main/kotlin/dev/alpas/inertia/InertiaBagBase.kt
@@ -4,12 +4,13 @@ import dev.alpas.auth.BaseUser
 import dev.alpas.http.HttpCall
 
 open class InertiaBagBase {
-    open operator fun invoke(call: HttpCall): Map<String, Map<String, Any?>> {
+    open operator fun invoke(call: HttpCall): Map<String, Any?> {
         val auth = userAsInertiaPayload(call)
         val errors = call.session.errors()
         val flash = call.session.userFlashBag()
+        val csrfToken = call.session.csrfToken()
 
-        return mapOf("errors" to errors, "flash" to flash, "auth" to auth)
+        return mapOf("errors" to errors, "flash" to flash, "auth" to auth, "csrf_token" to csrfToken)
     }
 
     protected open fun userAsInertiaPayload(call: HttpCall): Map<String, Any?> {
