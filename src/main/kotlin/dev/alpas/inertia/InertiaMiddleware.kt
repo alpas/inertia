20f01fc1ecc0f85c886014fc6d47c8430b4fdc8f
Ashok Gelal <ashok@metageek.net>
2020-04-22 20:14:44 -0600

    Only change 302 redirects

--- src/main/kotlin/dev/alpas/inertia/InertiaMiddleware.kt
+++ src/main/kotlin/dev/alpas/inertia/InertiaMiddleware.kt
@@ -6,14 +6,13 @@ import dev.alpas.http.HttpCall
 import dev.alpas.http.Method
 import dev.alpas.http.RedirectFilter
 import dev.alpas.http.RedirectResponse
-import dev.alpas.isOneOf
 
 class InertiaMiddleware : Middleware<HttpCall>() {
     @Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE")
     override fun invoke(call: HttpCall, forward: Handler<HttpCall>) {
         call.redirect().pushFilter(object : RedirectFilter() {
             override fun invoke(redirectResponse: RedirectResponse, forward: Handler<RedirectResponse>) {
-                if (call.method.isOneOf(Method.DELETE, Method.PUT, Method.PATCH)) {
+                if (redirectResponse.statusCode == 302 && call.method in listOf(Method.DELETE, Method.PUT, Method.PATCH)) {
                     redirectResponse.statusCode = 303
                 }
                 forward(redirectResponse)
