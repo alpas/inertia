8ddbc65775a73acbcc92d755dbdd9c5523845983
Ashok Gelal <ashok@metageek.net>
2020-04-22 20:14:18 -0600

    Pass extra params

--- src/main/kotlin/dev/alpas/inertia/InertiaBagBase.kt
+++ src/main/kotlin/dev/alpas/inertia/InertiaBagBase.kt
@@ -6,11 +6,12 @@ import dev.alpas.http.HttpCall
 open class InertiaBagBase {
     open operator fun invoke(call: HttpCall): Map<String, Any?> {
         val auth = userAsInertiaPayload(call)
+        val extras = call.shared("inertiaExtras")
         val errors = call.session.errors()
         val flash = call.session.userFlashBag()
         val csrfToken = call.session.csrfToken()
 
-        return mapOf("errors" to errors, "flash" to flash, "auth" to auth, "csrf_token" to csrfToken)
+        return mapOf("errors" to errors, "flash" to flash, "auth" to auth, "csrf_token" to csrfToken, "extras" to extras)
     }
 
     protected open fun userAsInertiaPayload(call: HttpCall): Map<String, Any?> {
@@ -20,7 +21,8 @@ open class InertiaBagBase {
                 "user" to mapOf(
                     "id" to user.id,
                     "name" to user.name,
-                    "email" to user.email
+                    "email" to user.email,
+                    "gravatar" to user.gravatarUrl()
                 )
             )
         } else {
